9898914026960e1cf917385133a7bcd7ef38f17f
grabania <grabania.work@gmail.com>
2020-03-22 19:16:59 +0000

    Add input & multi-line string examples

---
+++ styleandstructure.py
@@ -0,0 +1,12 @@
+story = ("Once upon a time there was a very long string that was "
+         "over 100 characters long and could not all fit on the "
+         "screen at once.")
+print(story)
+
+n = input("What's your favorite number?\n")
+if n == '7':
+    print("Mine too!")
+
+n = input("What's your favorite number?\n")
+if int(n) == 7:
+    print("Mine too!")
